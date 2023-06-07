97dab23b69107075ad9534777931a88cb4a49bee
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-06-07 22:59:37 +0700

    update

---
+++ app/Task/CheckLastConnection.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Task;
+
+use Carbon\Carbon;
+use App\Model\Fleet;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Crontab\Annotation\Crontab;
+use Hyperf\Contract\StdoutLoggerInterface;
+
+#[Crontab(name: "CheckLastConnection", rule: "* * * * *", callback: "execute", memo: "Check last connection & set disconnect")]
+class CheckLastConnection
+{
+     #[Inject]
+    private StdoutLoggerInterface $logger;
+
+    public function execute()
+    {
+        $now = Carbon::now();
+
+        Fleet::where('connected', 1)
+            ->where('last_connection', '<', $now->subMinutes(5)->format('Y-m-d H:i:s'))
+            ->update([
+                'connected' => 0
+            ]);
+    }
+}
--- config/autoload/crontab.php
+++ config/autoload/crontab.php
@@ -10,5 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    Hyperf\Crontab\Process\CrontabDispatcherProcess::class,
+    'enable' => true,
+    'crontab' => [
+    ],
 ];
