16f80e6a917545d5cc2e6dacedb4c4e2712d7ed9
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-06-23 20:24:59 +0700

    update

---
+++ app/Model/Engine/TypeP.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Model\Engine;
+
+use Carbon\Carbon;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Model\Model;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Model\Events\Updated;
+
+class TypeP extends Model
+{
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'engines';
+
+    /**
+     * The connection name for the model.
+     */
+    protected ?string $connection = 'default';
+
+    /**
+     * all 
+     */
+    protected array $guarded = ['id']; 
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [
+        'terminal_time' => 'datetime'
+    ];
+
+    // create table cargo if not found table
+    public static function table($fleetId)
+    {
+        $model = new self;
+        $tableName = $model->getTable() . "_{$fleetId}";
+        
+        if(! Schema::hasTable($tableName)) {
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('fleet_id')->index();
+                $table->datetime('terminal_time')->index();
+                $table->float('lo_inlet_pressure_ge1', 12, 5)->default(0);
+                $table->float('cfw_inlet_pressure_ge1', 12, 5)->default(0);
+                $table->float('fo_inlet_pressure_ge1', 12, 5)->default(0);
+                $table->float('lo_inlet_pressure_ge2', 12, 5)->default(0);
+                $table->float('cfw_inlet_pressure_ge2', 12, 5)->default(0);
+                $table->float('fo_inlet_pressure_ge2', 12, 5)->default(0);
+                $table->float('lo_inlet_pressure_ge3', 12, 5)->default(0);
+                $table->float('cfw_inlet_pressure_ge3', 12, 5)->default(0);
+                $table->float('fo_inlet_pressure_ge3', 12, 5)->default(0);
+                $table->float('exgas_tc_inlet_temp_ge1', 12, 5)->default(0);
+                $table->float('lo_inlet_temp_ge1', 12, 5)->default(0);
+                $table->float('cfw_outlet_temp_ge1', 12, 5)->default(0);
+                $table->float('exgas_tc_inlet_temp_ge2', 12, 5)->default(0);
+                $table->float('lo_inlet_temp_ge2', 12, 5)->default(0);
+                $table->float('cfw_outlet_temp_ge2', 12, 5)->default(0);
+                $table->float('exgas_tc_inlet_temp_ge3', 12, 5)->default(0);
+                $table->float('lo_inlet_temp_ge3', 12, 5)->default(0);
+                $table->float('cfw_outlet_temp_ge3', 12, 5)->default(0);
+                $table->float('sea_water_temp_ecr', 12, 5)->default(0);
+                $table->float('stern_tube_fwd_bearing_temp', 12, 5)->default(0);
+                $table->float('stern_tube_aft_bearing_temp', 12, 5)->default(0);
+                $table->float('intermediate_shaft_bearing_temp', 12, 5)->default(0);
+                $table->float('fo_service_tank1_temp', 12, 5)->default(0);
+                $table->float('fo_service_tank2_temp', 12, 5)->default(0);
+                $table->float('fo_service_tank3_temp', 12, 5)->default(0);
+                $table->float('me_fo_inlet_pressure', 12, 5)->default(0);
+                $table->float('tc_lo_inlet_pressure', 12, 5)->default(0);
+                $table->float('me_main_low_pco_inlet_pressure', 12, 5)->default(0);
+                $table->float('me_jacket_cw_inlet_pressure', 12, 5)->default(0);
+                $table->float('me_air_cooler_cw_inlet_pressure', 12, 5)->default(0);
+                $table->float('me_start_air_inlet_pressure', 12, 5)->default(0);
+                $table->float('me_scavenge_air_pressure', 12, 5)->default(0);
+                $table->float('me_control_air_inlet_pressure', 12, 5)->default(0);
+                $table->float('main_air_reservoir_1_pressure', 12, 5)->default(0);
+                $table->float('main_air_reservoir_2_pressure', 12, 5)->default(0);
+                $table->float('me_cyl1_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl2_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl3_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl4_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl5_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl6_jcw_outlet_temp', 12, 5)->default(0);
+                $table->float('me_jacket_cw_inlet_temp', 12, 5)->default(0);
+                $table->float('me_fo_inlet_temp', 12, 5)->default(0);
+                $table->float('me_thruster_bearing_temp', 12, 5)->default(0);
+                $table->float('me_tc_lo_outlet_temp', 12, 5)->default(0);
+                $table->float('me_scav_air_receiver_temp', 12, 5)->default(0);
+                $table->float('me_air_cooler_cw_inlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl1_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl2_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl3_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl4_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl5_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl6_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_tc_exhgas_inlet_temp', 12, 5)->default(0);
+                $table->float('me_tc_exhgas_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl1_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_cyl2_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_cyl3_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_cyl4_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_cyl5_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_cyl6_scav_air_fire_temp', 12, 5)->default(0);
+                $table->float('me_main_lo_pco_inlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl1_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl2_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl3_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl4_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl5_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl6_pco_outlet_temp', 12, 5)->default(0);
+                $table->float('me_cyl1_fore_main_bear_temp', 12, 5)->default(0);
+                $table->float('me_cyl1_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_cyl2_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_cyl3_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_cyl4_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_cyl5_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_cyl6_main__bearing_temp', 12, 5)->default(0);
+                $table->float('me_thrust_radial_bearing_temp', 12, 5)->default(0);
+                $table->float('control_air_inlet_pressure', 12, 5)->default(0);
+                $table->timestamps();
+            });
+        }
+        
+        return $model->setTable($tableName);
+    }
+
+
+    // update & insert
+    public function updated(Updated $event)
+    {
+        $model = $event->getModel();
+        $date = $model->terminal_time;
+        $last = TypePLog::table($model->fleet_id, $date)->orderBy('terminal_time', 'desc')->first();
+        $now = Carbon::parse($date);
+
+        // save interval 60 detik
+        if($last && $now->diffInSeconds($last->terminal_time) < config('mqtt.interval_save', 60) ) {   
+            return;
+        }
+
+        return TypePLog::table($model->fleet_id, $date)->updateOrCreate([
+            'fleet_id' => $model->fleet_id,
+            'terminal_time' => $date,
+        ], (array) $model->makeHidden(['id', 'fleet_id', 'created_at', 'updated_at'])->toArray());
+    }
+}
---
+++ app/Mqtt/PBrandan/Engine.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Mqtt\PBrandan;
+
+use Carbon\Carbon;
+use Hyperf\Utils\Codec\Json;
+
+class Engine
+{
+    protected string $message;
+
+    public function __construct(string $message) {
+        $this->message = $message;
+    }
+    
+    public function extract()
+    {
+        $data = Json::decode($this->message);
+        
+        return [
+            'engine' => [
+                'terminal_time' => (string) $data['_terminalTime'] ?: Carbon::now()->format('Y-m-d H:i:s'),
+                'lo_inlet_pressure_ge1' => (float) $data['lo_inlet_pressure_ge1'],
+                'cfw_inlet_pressure_ge1' => (float) $data['cfw_inlet_pressure_ge1'],
+                'fo_inlet_pressure_ge1' => (float) $data['fo_inlet_pressure_ge1'],
+                'lo_inlet_pressure_ge2' => (float) $data['lo_inlet_pressure_ge2'],
+                'cfw_inlet_pressure_ge2' => (float) $data['cfw_inlet_pressure_ge2'],
+                'fo_inlet_pressure_ge2' => (float) $data['fo_inlet_pressure_ge2'],
+                'lo_inlet_pressure_ge3' => (float) $data['lo_inlet_pressure_ge3'],
+                'cfw_inlet_pressure_ge3' => (float) $data['cfw_inlet_pressure_ge3'],
+                'fo_inlet_pressure_ge3' => (float) $data['fo_inlet_pressure_ge3'],
+                'exgas_tc_inlet_temp_ge1' => (float) $data['exgas_tc_inlet_temp_ge1'],
+                'lo_inlet_temp_ge1' => (float) $data['lo_inlet_temp_ge1'],
+                'cfw_outlet_temp_ge1' => (float) $data['cfw_outlet_temp_ge1'],
+                'exgas_tc_inlet_temp_ge2' => (float) $data['exgas_tc_inlet_temp_ge2'],
+                'lo_inlet_temp_ge2' => (float) $data['lo_inlet_temp_ge2'],
+                'cfw_outlet_temp_ge2' => (float) $data['cfw_outlet_temp_ge2'],
+                'exgas_tc_inlet_temp_ge3' => (float) $data['exgas_tc_inlet_temp_ge3'],
+                'lo_inlet_temp_ge3' => (float) $data['lo_inlet_temp_ge3'],
+                'cfw_outlet_temp_ge3' => (float) $data['cfw_outlet_temp_ge3'],
+                'sea_water_temp_ecr' => (float) $data['sea_water_temp_ecr'],
+                'stern_tube_fwd_bearing_temp' => (float) $data['stern_tube_fwd_bearing_temp'],
+                'stern_tube_aft_bearing_temp' => (float) $data['stern_tube_aft_bearing_temp'],
+                'intermediate_shaft_bearing_temp' => (float) $data['intermediate_shaft_bearing_temp'],
+                'fo_service_tank1_temp' => (float) $data['fo_service_tank1_temp'],
+                'fo_service_tank2_temp' => (float) $data['fo_service_tank2_temp'],
+                'fo_service_tank3_temp' => (float) $data['fo_service_tank3_temp'],
+                'me_fo_inlet_pressure' => (float) $data['me_fo_inlet_pressure'],
+                'tc_lo_inlet_pressure' => (float) $data['tc_lo_inlet_pressure'],
+                'me_main_low_pco_inlet_pressure' => (float) $data['me_main_low_pco_inlet_pressure'],
+                'me_jacket_cw_inlet_pressure' => (float) $data['me_jacket_cw_inlet_pressure'],
+                'me_air_cooler_cw_inlet_pressure' => (float) $data['me_air_cooler_cw_inlet_pressure'],
+                'me_start_air_inlet_pressure' => (float) $data['me_start_air_inlet_pressure'],
+                'me_scavenge_air_pressure' => (float) $data['me_scavenge_air_pressure'],
+                'me_control_air_inlet_pressure' => (float) $data['me_control_air_inlet_pressure'],
+                'main_air_reservoir_1_pressure' => (float) $data['main_air_reservoir_1_pressure'],
+                'main_air_reservoir_2_pressure' => (float) $data['main_air_reservoir_2_pressure'],
+                'me_cyl1_jcw_outlet_temp' => (float) $data['me_cyl1_jcw_outlet_temp'],
+                'me_cyl2_jcw_outlet_temp' => (float) $data['me_cyl2_jcw_outlet_temp'],
+                'me_cyl3_jcw_outlet_temp' => (float) $data['me_cyl3_jcw_outlet_temp'],
+                'me_cyl4_jcw_outlet_temp' => (float) $data['me_cyl4_jcw_outlet_temp'],
+                'me_cyl5_jcw_outlet_temp' => (float) $data['me_cyl5_jcw_outlet_temp'],
+                'me_cyl6_jcw_outlet_temp' => (float) $data['me_cyl6_jcw_outlet_temp'],
+                'me_jacket_cw_inlet_temp' => (float) $data['me_jacket_cw_inlet_temp'],
+                'me_fo_inlet_temp' => (float) $data['me_fo_inlet_temp'],
+                'me_thruster_bearing_temp' => (float) $data['me_thruster_bearing_temp'],
+                'me_tc_lo_outlet_temp' => (float) $data['me_tc_lo_outlet_temp'],
+                'me_scav_air_receiver_temp' => (float) $data['me_scav_air_receiver_temp'],
+                'me_air_cooler_cw_inlet_temp' => (float) $data['me_air_cooler_cw_inlet_temp'],
+                'me_cyl1_exhgas_outlet_temp' => (float) $data['me_cyl1_exhgas_outlet_temp'],
+                'me_cyl2_exhgas_outlet_temp' => (float) $data['me_cyl2_exhgas_outlet_temp'],
+                'me_cyl3_exhgas_outlet_temp' => (float) $data['me_cyl3_exhgas_outlet_temp'],
+                'me_cyl4_exhgas_outlet_temp' => (float) $data['me_cyl4_exhgas_outlet_temp'],
+                'me_cyl5_exhgas_outlet_temp' => (float) $data['me_cyl5_exhgas_outlet_temp'],
+                'me_cyl6_exhgas_outlet_temp' => (float) $data['me_cyl6_exhgas_outlet_temp'],
+                'me_tc_exhgas_inlet_temp' => (float) $data['me_tc_exhgas_inlet_temp'],
+                'me_tc_exhgas_outlet_temp' => (float) $data['me_tc_exhgas_outlet_temp'],
+                'me_cyl1_scav_air_fire_temp' => (float) $data['me_cyl1_scav_air_fire_temp'],
+                'me_cyl2_scav_air_fire_temp' => (float) $data['me_cyl2_scav_air_fire_temp'],
+                'me_cyl3_scav_air_fire_temp' => (float) $data['me_cyl3_scav_air_fire_temp'],
+                'me_cyl4_scav_air_fire_temp' => (float) $data['me_cyl4_scav_air_fire_temp'],
+                'me_cyl5_scav_air_fire_temp' => (float) $data['me_cyl5_scav_air_fire_temp'],
+                'me_cyl6_scav_air_fire_temp' => (float) $data['me_cyl6_scav_air_fire_temp'],
+                'me_main_lo_pco_inlet_temp' => (float) $data['me_main_lo_pco_inlet_temp'],
+                'me_cyl1_pco_outlet_temp' => (float) $data['me_cyl1_pco_outlet_temp'],
+                'me_cyl2_pco_outlet_temp' => (float) $data['me_cyl2_pco_outlet_temp'],
+                'me_cyl3_pco_outlet_temp' => (float) $data['me_cyl3_pco_outlet_temp'],
+                'me_cyl4_pco_outlet_temp' => (float) $data['me_cyl4_pco_outlet_temp'],
+                'me_cyl5_pco_outlet_temp' => (float) $data['me_cyl5_pco_outlet_temp'],
+                'me_cyl6_pco_outlet_temp' => (float) $data['me_cyl6_pco_outlet_temp'],
+                'me_cyl1_fore_main_bear_temp' => (float) $data['me_cyl1_fore_main_bear_temp'],
+                'me_cyl1_main__bearing_temp' => (float) $data['me_cyl1_main__bearing_temp'],
+                'me_cyl2_main__bearing_temp' => (float) $data['me_cyl2_main__bearing_temp'],
+                'me_cyl3_main__bearing_temp' => (float) $data['me_cyl3_main__bearing_temp'],
+                'me_cyl4_main__bearing_temp' => (float) $data['me_cyl4_main__bearing_temp'],
+                'me_cyl5_main__bearing_temp' => (float) $data['me_cyl5_main__bearing_temp'],
+                'me_cyl6_main__bearing_temp' => (float) $data['me_cyl6_main__bearing_temp'],
+                'me_thrust_radial_bearing_temp' => (float) $data['me_thrust_radial_bearing_temp'],
+                'control_air_inlet_pressure' => (float) $data['control_air_inlet_pressure'],
+            ]
+        ];
+    }
+}
