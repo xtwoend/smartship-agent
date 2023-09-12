431f896fc9cd0df632a2c3e434d0a5b8196b4101
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-09-12 14:28:19 +0700

    update

--- app/Model/Engine/EngineTrait.php
+++ app/Model/Engine/EngineTrait.php
@@ -5,11 +5,11 @@ namespace App\Model\Engine;
 trait EngineTrait
 {
     public function setEngine($model, array $data)
-    {   
+    {    
         if(isset($data['engine'])) {
             
             $model = (new $model)->table($this->id);
-
+            
             $log = $model->updateOrCreate([
                 'fleet_id' => $this->id
             ], $data['engine']);
