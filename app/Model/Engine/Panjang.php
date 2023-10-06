47b89eca555b55f12fdbeba80e68420fcffe3fea
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-06 09:27:50 +0700

    update

---
+++ app/Model/Engine/Panjang.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Model\Engine;
+
+use Carbon\Carbon;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Model\Model;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Model\Events\Updated;
+
+class Panjang extends Model
+{
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'engines';
+
+    /**
+     * The connection name for the model.
+     */
+    protected ?string $connection = 'default';
+
+    /**
+     * all 
+     */
+    protected array $guarded = ['id']; 
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [
+        'terminal_time' => 'datetime'
+    ];
+
+    // create table cargo if not found table
+    public static function table($fleetId)
+    {
+        $model = new self;
+        $tableName = $model->getTable() . "_{$fleetId}";
+       
+        if(! Schema::hasTable($tableName)) {
+            
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('fleet_id')->index();
+                $table->datetime('terminal_time')->index();
+                $table->float('me_exh_vv_spring_air_pressure', 10, 3)->default(0);
+                $table->float('me_fo_inlet_pressure', 10, 3)->default(0);
+                $table->float('tc_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_air_cooler_cw_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_exh_vv_driv_oil_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_jacket_cfw_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_starting_air_pressure', 10, 3)->default(0);
+                $table->float('me_scavenging_air_pressure', 10, 3)->default(0);
+                $table->float('no1_ge_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('no2_ge_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('no3_ge_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_lo_inlet_pressure', 10, 3)->default(0);
+                $table->timestamps();
+            });
+        }
+        
+        return $model->setTable($tableName);
+    }
+
+    // update & insert
+    public function updated(Updated $event)
+    {
+        $model = $event->getModel();
+        $date = $model->terminal_time;
+        $last = PanjangLog::table($model->fleet_id, $date)->orderBy('terminal_time', 'desc')->first();
+        $now = Carbon::parse($date);
+
+        // save interval 60 detik
+        if($last && $now->diffInSeconds($last->terminal_time) < config('mqtt.interval_save', 60) ) {   
+            return;
+        }
+
+        return PanjangLog::table($model->fleet_id, $date)->updateOrCreate([
+            'fleet_id' => $model->fleet_id,
+            'terminal_time' => $date,
+        ], (array) $model->makeHidden(['id', 'fleet_id', 'created_at', 'updated_at'])->toArray());
+    }
+}
---
+++ app/Mqtt/Panjang/Engine.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mqtt\Panjang;
+
+use Carbon\Carbon;
+use Hyperf\Utils\Codec\Json;
+
+class Engine
+{
+    protected string $message;
+
+    public function __construct(string $message) {
+       
+        $this->message = $message;
+    }
+    
+    public function extract()
+    {
+        $data = Json::decode($this->message);
+        
+        return [
+            'engine' => [
+                'terminal_time' => $data['_terminalTime'] ?? Carbon::now()->format('Y-m-d H:i:s'),
+                'me_exh_vv_spring_air_pressure' => $data['me_exh_vv_spring_air_pressure'],
+                'me_fo_inlet_pressure' => $data['me_fo_inlet_pressure'],
+                'tc_lo_inlet_pressure' => $data['tc_lo_inlet_pressure'],
+                'me_air_cooler_cw_inlet_pressure' => $data['me_air_cooler_cw_inlet_pressure'],
+                'me_exh_vv_driv_oil_inlet_pressure' => $data['me_exh_vv_driv_oil_inlet_pressure'],
+                'me_jacket_cfw_inlet_pressure' => $data['me_jacket_cfw_inlet_pressure'],
+                'me_starting_air_pressure' => $data['me_starting_air_pressure'],
+                'me_scavenging_air_pressure' => $data['me_scavenging_air_pressure'],
+                'no1_ge_lo_inlet_pressure' => $data['no1_ge_lo_inlet_pressure'],
+                'no2_ge_lo_inlet_pressure' => $data['no2_ge_lo_inlet_pressure'],
+                'no3_ge_lo_inlet_pressure' => $data['no3_ge_lo_inlet_pressure'],
+                'me_lo_inlet_pressure' => $data['me_lo_inlet_pressure'],
+            ]
+        ];
+    }
+}
