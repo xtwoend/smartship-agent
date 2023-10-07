9e8626611d51724c4205d6d8d10cf4a2e4f3934a
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-07 18:00:59 +0700

    update

---
+++ app/Model/Engine/PangrangoLog.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Model\Engine;
+
+use Carbon\Carbon;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Model\Model;
+use App\Model\Alarm\SensorAlarmTrait;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Model\Events\Updated;
+
+class PangrangoLog extends Model
+{
+    use SensorAlarmTrait;
+
+    /**
+     * engine group sensor
+     */
+    public array $sensor_group = ['engine'];
+     
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'engine_log';
+
+    /**
+     * The connection name for the model.
+     */
+    protected ?string $connection = 'default';
+
+    /**
+     * all 
+     */
+    protected array $guarded = ['id']; 
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [
+        'terminal_time' => 'datetime'
+    ];
+
+    // create table cargo if not found table
+    public static function table($fleetId, $date = null)
+    {
+        $date = is_null($date) ? date('Ym'): Carbon::parse($date)->format('Ym');
+        $model = new self;
+        $tableName = $model->getTable() . "_{$fleetId}_{$date}";
+        
+        if(! Schema::hasTable($tableName)) {
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('fleet_id')->index();
+                $table->datetime('terminal_time')->index();
+                $table->float('me_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('tc_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('jcw_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_fo_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_starting_air_pressure', 10, 3)->default(0);
+                $table->float('me_exh_vv_drive_oil_inlet_pressure', 10, 3)->default(0);
+                $table->float('me_scav_air_pressure', 10, 3)->default(0);
+                $table->float('no1_dg_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('no2_dg_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('no3_dg_lo_inlet_pressure', 10, 3)->default(0);
+                $table->float('no1_main_air_bottle_pressure', 10, 3)->default(0);
+                $table->float('no2_main_air_bottle_pressure', 10, 3)->default(0);
+                $table->timestamps();
+            });
+        }
+        
+        return $model->setTable($tableName);
+    }
+}
---
+++ app/Mqtt/Pangrango/Engine.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mqtt\Pagerungan;
+
+use Carbon\Carbon;
+use Hyperf\Utils\Codec\Json;
+
+class Engine
+{
+    protected string $message;
+
+    public function __construct(string $message) {
+       
+        $this->message = $message;
+    }
+    
+    public function extract()
+    {
+        $data = Json::decode($this->message);
+        
+        return [
+            'engine' => [
+                'terminal_time' => Carbon::now()->format('Y-m-d H:i:s'),
+                'me_lo_inlet_pressure' => $data['me_lo_inlet_pressure'],
+                'tc_lo_inlet_pressure' => $data['tc_lo_inlet_pressure'],
+                'jcw_inlet_pressure' => $data['jcw_inlet_pressure'],
+                'me_fo_inlet_pressure' => $data['me_fo_inlet_pressure'],
+                'me_starting_air_pressure' => $data['me_starting_air_pressure'],
+                'me_exh_vv_drive_oil_inlet_pressure' => $data['me_exh_vv_drive_oil_inlet_pressure'],
+                'me_scav_air_pressure' => $data['me_scav_air_pressure'],
+                'no1_dg_lo_inlet_pressure' => $data['no1_dg_lo_inlet_pressure'],
+                'no2_dg_lo_inlet_pressure' => $data['no2_dg_lo_inlet_pressure'],
+                'no3_dg_lo_inlet_pressure' => $data['no3_dg_lo_inlet_pressure'],
+                'no1_main_air_bottle_pressure' => $data['no1_main_air_bottle_pressure'],
+                'no2_main_air_bottle_pressure' => $data['no2_main_air_bottle_pressure'],
+            ]
+        ];
+    }
+}
