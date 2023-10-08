6a968dfe1092173d2cd9b4109026fa1525c6995a
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-08 08:14:05 +0700

    update

--- app/Model/Alarm/SensorAlarmTrait.php
+++ app/Model/Alarm/SensorAlarmTrait.php
@@ -19,7 +19,7 @@ trait SensorAlarmTrait
     {
         $model = $event->getModel();
         $fleetId = $model->fleet_id;
-        foreach($this->sensor()->whereIn('group', $this->sensor_group)->get() as $sensor) {
+        foreach($this->sensor()->whereIn('group', $this->sensor_group)->where('is_ams', 1)->get() as $sensor) {
             $val = $model->{$sensor->sensor_name};
             $val = round($val, 2, PHP_ROUND_HALF_UP);
             
