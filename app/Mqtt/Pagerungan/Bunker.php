f1035db0143dc8bfcebf4efbf37dcf937df07e98
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-10 13:37:15 +0700

    update

--- app/Mqtt/Pagerungan/Bunker.php
+++ app/Mqtt/Pagerungan/Bunker.php
@@ -20,19 +20,19 @@ class Bunker
         
         return [
             'engine' => [
-                'terminal_time' => Carbon::now()->format('Y-m-d H:i:s'),
-                'hfo_storage_tank_1p' => (float) $data['hfo_storage_tank_1p'],
-                'hfo_storage_tank_1s' => (float) $data['hfo_storage_tank_1s'],
-                'hfo_storage_tank_2p' => (float) $data['hfo_storage_tank_2p'],
-                'hfo_storage_tank_2s' => (float) $data['hfo_storage_tank_2s'],
-                'hfo_setting_tank' => (float) $data['hfo_setting_tank'],
-                'hfo_service_tank_1' => (float) $data['hfo_service_tank_1'],
-                'hfo_service_tank_2' => (float) $data['hfo_service_tank_2'],
-                'mdo_storage_tank_p' => (float) $data['mdo_storage_tank_p'],
-                'mdo_storage_tank_s' => (float) $data['mdo_storage_tank_s'],
-                'mdo_setting_tank' => (float) $data['mdo_setting_tank'],
-                'mdo_service_tank_1' => (float) $data['mdo_service_tank_1'],
-                'mdo_service_tank_2' => (float) $data['mdo_service_tank_2'],
+                // 'terminal_time' => Carbon::now()->format('Y-m-d H:i:s'),
+                'hfo_storage_tank_1p' => $data['hfo_storage_tank_1p'],
+                'hfo_storage_tank_1s' => $data['hfo_storage_tank_1s'],
+                'hfo_storage_tank_2p' => $data['hfo_storage_tank_2p'],
+                'hfo_storage_tank_2s' => $data['hfo_storage_tank_2s'],
+                'hfo_setting_tank' => $data['hfo_setting_tank'],
+                'hfo_service_tank_1' => $data['hfo_service_tank_1'],
+                'hfo_service_tank_2' => $data['hfo_service_tank_2'],
+                'mdo_storage_tank_p' => $data['mdo_storage_tank_p'],
+                'mdo_storage_tank_s' => $data['mdo_storage_tank_s'],
+                'mdo_setting_tank' => $data['mdo_setting_tank'],
+                'mdo_service_tank_1' => $data['mdo_service_tank_1'],
+                'mdo_service_tank_2' => $data['mdo_service_tank_2'],
             ]
         ];
     }
