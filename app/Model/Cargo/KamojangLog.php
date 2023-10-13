906b097d80cf8490885254a1ac9e8e30d8766084
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-10-13 09:18:01 +0700

    update

---
+++ app/Model/Cargo/KamojangLog.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Cargo;
+
+use Carbon\Carbon;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Model\Model;
+use App\Model\Alarm\SensorAlarmTrait;
+use Hyperf\Database\Schema\Blueprint;
+
+class KamojangLog extends Model
+{
+    use SensorAlarmTrait;
+
+    /**
+     * engine group sensor
+     */
+    public array $sensor_group = ['cargo'];
+    
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'cargo_log';
+
+    /**
+     * The connection name for the model.
+     */
+    protected ?string $connection = 'default';
+
+    /**
+     * all 
+     */
+    protected array $guarded = ['id']; 
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [
+        'terminal_time' => 'datetime',
+    ];
+
+    // create table cargo if not found table
+    public static function table($fleetId, $date = null)
+    {
+        $date = is_null($date) ? date('Ym'): Carbon::parse($date)->format('Ym');
+        $model = new self;
+        $tableName = $model->getTable() . "_{$fleetId}_{$date}";
+        
+        if(! Schema::hasTable($tableName)) {
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->unsignedBigInteger('fleet_id')->index();
+                $table->datetime('terminal_time')->unique();
+                
+                $table->float('no_1_cargo_tank_p', 10, 3)->default(0);
+                $table->float('temp_1ctp', 10, 3)->default(0);
+                $table->float('no_1_cargo_tank_s', 10, 3)->default(0);
+                $table->float('temp_1cts', 10, 3)->default(0);
+                $table->float('no_2_cargo_tank_p', 10, 3)->default(0);
+                $table->float('temp_2ctp', 10, 3)->default(0);
+                $table->float('no_2_cargo_tank_s', 10, 3)->default(0);
+                $table->float('temp_2cts', 10, 3)->default(0);
+                $table->float('no_3_cargo_tank_p', 10, 3)->default(0);
+                $table->float('temp_3ctp', 10, 3)->default(0);
+                $table->float('no_3_cargo_tank_s', 10, 3)->default(0);
+                $table->float('temp_3ctm', 10, 3)->default(0);
+                $table->float('no_4_cargo_tank_p', 10, 3)->default(0);
+                $table->float('temp_4ctp', 10, 3)->default(0);
+                $table->float('no_4_cargo_tank_s', 10, 3)->default(0);
+                $table->float('temp_4cts', 10, 3)->default(0);
+                $table->float('no_5_cargo_tank_p', 10, 3)->default(0);
+                $table->float('temp_5ctp', 10, 3)->default(0);
+                $table->float('no_5_cargo_tank_s', 10, 3)->default(0);
+                $table->float('temp_5cts', 10, 3)->default(0);
+                $table->float('slop_tank_p', 10, 3)->default(0);
+                $table->float('temp_stp', 10, 3)->default(0);
+                $table->float('slop_tank_s', 10, 3)->default(0);
+                $table->float('temp_sts', 10, 3)->default(0);
+
+                // pump status
+                $table->datetime('pump_timestamp')->nullable();
+                $table->boolean('cargo_pump1_run')->nullable();
+                $table->boolean('cargo_pump2_run')->nullable();
+                $table->boolean('cargo_pump3_run')->nullable();
+                $table->boolean('wballast_pump1_run')->nullable();
+                $table->boolean('wballast_pump2_run')->nullable();
+                $table->boolean('tank_cleanning_pump_run')->nullable();
+                $table->boolean('stripping_pump1_run')->nullable();
+                $table->boolean('stripping_pump2_run')->nullable();
+
+                $table->timestamps();
+            });
+        }
+        
+        return $model->setTable($tableName);
+    }
+}
---
+++ app/Mqtt/Kamojang/PumpStatus.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Mqtt\Kamojang;
+
+use Carbon\Carbon;
+use Hyperf\Utils\Str;
+use Hyperf\Utils\Codec\Json;
+
+class PumpStatus
+{
+    protected string $message;
+
+    public function __construct(string $message) {
+       
+        $this->message = $message;
+    }
+    
+    public function extract()
+    {
+        $data = Json::decode($this->message);
+        
+        return [
+            'cargo' => [
+                'pump_timestamp' => $data['_terminalTime'] ?: Carbon::now()->format('Y-m-d H:i:s'),
+                'cargo_pump1_run' => $data['cargo_pump1_run'],
+                'cargo_pump2_run' => $data['cargo_pump2_run'],
+                'cargo_pump3_run' => $data['cargo_pump3_run'],
+                'wballast_pump1_run' => $data['wballast_pump1_run'],
+                'wballast_pump2_run' => $data['wballast_pump2_run'],
+                'tank_cleanning_pump_run' => $data['tank_cleanning_pump_run'],
+                'stripping_pump1_run' => $data['stripping_pump1_run'],
+                'stripping_pump2_run' => $data['stripping_pump2_run'],
+            ]
+        ];
+    }
+
+    function arrayToSnake() : array {
+        $snake = [];
+        foreach($this->mappArray as $in => $val) {
+            if(is_null($val)) continue;
+            $key = Str::snake(strtolower($val));
+            $snake[$key] = $val;
+        } 
+        return $snake;
+    }
+
+    protected $mappArray = [
+    ];
+}
