84ab66e3320c9d3c428ed95a5af20c576df67d2d
Abdul Hafidz Anshari <aditans88@gmail.com>
2023-11-13 21:49:26 +0700

    update

---
+++ app/Model/FleetDailyReport.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Carbon\Carbon;
+use Hyperf\Database\Schema\Schema;
+use Hyperf\DbConnection\Model\Model;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Model\Events\Creating;
+
+/**
+ */
+class FleetDailyReport extends Model
+{
+    /**
+     * The table associated with the model.
+     */
+    protected ?string $table = 'fleet_daily_reports';
+
+    /**
+     * timestamps false
+     */
+    public bool $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected array $fillable = ['fleet_id', 'date', 'sensor', 'value'];
+
+    /**
+     * The attributes that should be cast to native types.
+     */
+    protected array $casts = [
+        'date' => 'date'
+    ];
+
+    public static function table($fleetId, $date = null)
+    {
+        $date = is_null($date) ? date('Ym'): Carbon::parse($date)->format('Ym');
+        $model = new self;
+        $tableName = $model->getTable() . "_{$fleetId}_{$date}";
+        
+        if(! Schema::hasTable($tableName)) {
+            Schema::create($tableName, function (Blueprint $table) {
+                $table->uuid('id')->primary();
+                $table->unsignedBigInteger('fleet_id')->index();
+                $table->date('date')->unique();
+                $table->string('sensor')->nullable();
+                $table->string('value')->nullable();
+                // $table->timestamps();
+            });
+        }
+        
+        return $model->setTable($tableName);
+    }
+
+    public function creating(Creating $event)
+    {
+        $this->id = \Ramsey\Uuid\Uuid::uuid4()->toString();
+    }
+}
