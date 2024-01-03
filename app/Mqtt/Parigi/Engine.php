ecdbf57338d93341b80754a083e5978e3fc606ea
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-01-03 20:55:09 +0700

    update

--- app/Mqtt/Parigi/Engine.php
+++ app/Mqtt/Parigi/Engine.php
@@ -21,18 +21,18 @@ class Engine
         return  [
             'engine' => [
                 'terminal_time' => Carbon::now()->format('Y-m-d H:i:s'),
-                'no1_ge_fo_inlet_press_low' => (float) $data['no1_ge_fo_inlet_press_low'],
-                'me_jacket_cfw_inlet_press' => (float) $data['me_jacket_cfw_inlet_press'],
-                'no2_ge_cfw_inlet_press_low' => (float) $data['no2_ge_cfw_inlet_press_low'],
-                'starting_air_press_ecr' => (float) $data['starting_air_press_ecr'],
-                'starting_air_press_low' => (float) $data['starting_air_press_low'],
-                'tc_lo_inlet_press_low' => (float) $data['tc_lo_inlet_press_low'],
-                'mlo_pco_inlet_press_ecr' => (float) $data['mlo_pco_inlet_press_ecr'],
-                'fo_inlet_press_low' => (float) $data['fo_inlet_press_low'],
-                'me_air_cooler_cw_inlet_press' => (float) $data['me_air_cooler_cw_inlet_press'],
-                'no1_ge_lo_inlet_press' => (float) $data['no1_ge_lo_inlet_press'],
-                'no2_ge_lo_inlet_press' => (float) $data['no2_ge_lo_inlet_press'],
-                'no3_ge_lo_inlet_press' => (float) $data['no3_ge_lo_inlet_press']
+                'no1_ge_fo_inlet_press_low' => (float) ($data['no1_ge_fo_inlet_press_low'] / 100),
+                'me_jacket_cfw_inlet_press' => (float) ($data['me_jacket_cfw_inlet_press'] / 100),
+                'no2_ge_cfw_inlet_press_low' => (float) ($data['no2_ge_cfw_inlet_press_low'] / 100),
+                'starting_air_press_ecr' => (float) ($data['starting_air_press_ecr'] / 100),
+                'starting_air_press_low' => (float) ($data['starting_air_press_low'] / 100),
+                'tc_lo_inlet_press_low' => (float) ($data['tc_lo_inlet_press_low'] / 100),
+                'mlo_pco_inlet_press_ecr' => (float) ($data['mlo_pco_inlet_press_ecr'] / 100),
+                'fo_inlet_press_low' => (float) ($data['fo_inlet_press_low'] / 100),
+                'me_air_cooler_cw_inlet_press' => (float) ($data['me_air_cooler_cw_inlet_press'] / 100),
+                'no1_ge_lo_inlet_press' => (float) ($data['no1_ge_lo_inlet_press'] / 100),
+                'no2_ge_lo_inlet_press' => (float) ($data['no2_ge_lo_inlet_press'] / 100),
+                'no3_ge_lo_inlet_press' => (float) ($data['no3_ge_lo_inlet_press'] / 100),
             ]
         ];
     }
