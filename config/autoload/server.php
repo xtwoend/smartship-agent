257548018510e2f09ed8f1a7a74ef16ba9b2e6ef
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-03-28 09:27:13 +0700

    update

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -41,10 +41,10 @@ return [
     ],
     'settings' => [
         Constant::OPTION_ENABLE_COROUTINE => true,
-        Constant::OPTION_WORKER_NUM => swoole_cpu_num(),
+        Constant::OPTION_WORKER_NUM => (int) (swoole_cpu_num() / 2),
         Constant::OPTION_PID_FILE => BASE_PATH . '/runtime/hyperf.pid',
         Constant::OPTION_OPEN_TCP_NODELAY => true,
-        Constant::OPTION_MAX_COROUTINE => 100000,
+        Constant::OPTION_MAX_COROUTINE => 1000000,
         Constant::OPTION_OPEN_HTTP2_PROTOCOL => true,
         Constant::OPTION_MAX_REQUEST => 100000,
         Constant::OPTION_SOCKET_BUFFER_SIZE => 2 * 1024 * 1024,
