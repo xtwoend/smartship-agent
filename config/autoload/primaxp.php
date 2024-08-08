10729806850f7f924b094922d03f1bef6aa69bf1
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-08-08 13:09:20 +0700

    update

--- config/autoload/primaxp.php
+++ config/autoload/primaxp.php
@@ -4,7 +4,7 @@ use function Hyperf\Support\env;
 
 return [
     'enable' => true,
-    'fleet_id' => 29,
+    'fleet_id' => 44,
     'fleet_model' => App\Model\Fleet::class,
     'logger' => App\Model\ErrorLog::class,
     'mqtt_connection' => [
