5e1ae2398e48d51cb413448614b4882c76ce0866
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-08-24 11:17:12 +0700

    update

--- fleets/gasparta2/src/Parser/VDR.php
+++ fleets/gasparta2/src/Parser/VDR.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-namespace Smartship\Aquila\Parser;
+namespace Smartship\Gasparta2\Parser;
 
 use Carbon\Carbon;
 
