2f3935910e0490f1ef79a1286179f9fb6e328b3d
taufiq <TaufiqHidayat@bitbucket.org>
2025-02-27 15:21:21 +0700

    update: cargo sounding

--- app/Model/CargoSounding.php
+++ app/Model/CargoSounding.php
@@ -49,9 +49,11 @@ class CargoSounding extends Model
                 $table->unsignedBigInteger('tank_id')->index();
                 $table->float('trim_index', 10, 3)->index()->nullable()->default(NULL);
                 $table->float('heel_index', 10, 3)->index()->nullable()->default(NULL); // (-) = port, 0 = no heel, (+) = stb
-                $table->unsignedInteger('mes_type')->index(); // level and ullage
-                $table->unsignedInteger('unit')->index(); // level or ullage value
-                $table->float('value', 10, 3)->nullable();
+                // $table->unsignedInteger('mes_type')->index(); // level and ullage
+                // $table->unsignedInteger('unit')->index(); // level or ullage value
+                $table->float('level', 10, 3)->nullable();
+                $table->float('ullage', 10, 3)->nullable();
+                $table->float('volume', 10, 3)->nullable(); // satuan m3
                 $table->float('diff', 10, 3)->nullable()->default(NULL);
                 $table->timestamps();
             });
