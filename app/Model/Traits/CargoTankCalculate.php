524b50c71bc51c3a11d2d5486a3bf87f1005222a
Cheetah <Cheetah@localhost>
2025-03-03 14:20:52 +0700

    update cargo calc

--- app/Model/Traits/CargoTankCalculate.php
+++ app/Model/Traits/CargoTankCalculate.php
@@ -5,9 +5,7 @@ namespace App\Model\Traits;
 
 use App\Model\CargoDensity;
 use App\Model\CargoSounding;
-use App\Model\CargoTankTable;
 use App\Model\CargoTankCorrection;
-use Hyperf\Database\Model\Events\Creating;
 
 
 trait CargoTankCalculate
@@ -17,47 +15,41 @@ trait CargoTankCalculate
     public function calculate($model)
     {
         $fleetId = $model->fleet_id;
-        // $tabelTank = CargoTankTable::table($fleetId)->get();
-        // var_dump($fleetId);
         $correctionTable = CargoTankCorrection::table($fleetId)->get();
         $soundingModel = CargoSounding::table($fleetId);
         $densityModel = new CargoDensity;
         $data = [];
         $cargos = ($model->cargos->count() < 1) ? $model->getCargos() : $model->cargos;
         foreach ($cargos as $key => $tank) {
-            // $ullage = $model->{$tank[0]};
-            // $ullage = $ullage < 0 ? 0 : $ullage;
-
-            // $temp = $model->{$tank[1]};
-            // $volRow = $tabelTank->where('tank_position', $tank[0])->where('ullage', $ullage)->first();
-            // tank position
-
-            // $correctionRow = $tabelCorrection->where('temp', $temp)->first();
-            // $vol = $volRow?->volume ?? 0;
-            // $correction = $correctionRow?->correction ?? 0;
-            // $total_obs = $vol * $correction;
-
-            // $data[$key] = $total_obs;
 
             // ullage are in M, convert to CM
             $unit = max(0, round($model->{$tank->tank_position} * 100, 1, PHP_ROUND_HALF_EVEN));
             $unitDecimal = round(fmod($unit, 1), 10);
-            $unit = round($unit);
+
             $trim = 0;
-            $temp = round($model->{"{$tank->tank_position}_temp"});
-            if ($tank->tank_locator === 'S') {
+            $heel = 0;
+            $correction = 1;
+            $temp = round(($model->{"{$tank->tank_position}_temp"} ?? 0));
+            $hasDraft = (isset($model->draft_fore) || isset($model->draft_rear) || isset($model->draft_center_left) || isset($model->draft_center_right)) ? true : false;
+
+            if ($hasDraft) {
+                // if ($tank->tank_locator === 'S') {
                 $fore = $model->draft_fore ?? $model->draft_front ?? 0;
                 $after = $model->draft_after ?? $model->draft_rear ?? 0;
                 $trim = round($fore - $after, 1, PHP_ROUND_HALF_EVEN);
-            }
-            $heel = round($model->draft_center_left - $model->draft_center_right, 1, PHP_ROUND_HALF_EVEN);
 
+                // }
+                $heel = round($model->draft_center_left - $model->draft_center_right, 1, PHP_ROUND_HALF_EVEN);
+            }
             if ($tank->mes_type == 'level' && $tank->height > 0) {
                 $unit = $tank->height - $unit;
             }
+            $unit = round($unit);
             $interpolatedVolume = 0;
-            $correctionRow = $correctionTable->where('temp', $temp)->first();
-            $correction = $correctionRow?->correction ?? 0;
+            if ($temp > 0) {
+                $correctionRow = $correctionTable->where('temp', $temp)->first();
+                $correction = $correctionRow?->correction ?? 1;
+            }
 
             if ($tank->calc_type != 'match') {
                 $volRow = $soundingModel->where('trim_index', 0)->where('heel_index', 0)->where('ullage', $unit)->first();
@@ -72,8 +64,12 @@ trait CargoTankCalculate
             $totalObs = ($interpolatedVolume + $vol) * $correction;
 
             $densityRow = $densityModel->where('product', $tank->content_type)->first();
-            $density = $densityRow?->density ?? 1;
-            $tonnaseObs = ($density * $totalObs);
+            $tonnaseObs = null;
+            if ($densityRow) {
+                $density = $densityRow?->density ?? 1;
+                $tonnaseObs = ($density * $totalObs);
+            }
+
             $data["{$tank->tank_position}_ltr"] = ($totalObs * 1000);
             $data["{$tank->tank_position}_mt"] = $tonnaseObs;
         }
