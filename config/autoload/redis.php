97ae37d7a74a604cf8963e604f1c9fe7427a2c72
Abdul Hafidz Anshari <aditans88@gmail.com>
2025-10-07 15:01:50 +0700

    update

--- config/autoload/redis.php
+++ config/autoload/redis.php
@@ -36,11 +36,15 @@ return [
         ],
         'pool' => [
             'min_connections' => 1,
-            'max_connections' => 100,
-            'connect_timeout' => 10.0,
+            'max_connections' => 5,
+            'connect_timeout' => 5.0,
             'wait_timeout' => 3.0,
-            'heartbeat' => -1,
-            'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 60),
+            'heartbeat' => 30,
+            'max_idle_time' => (float) env('REDIS_MAX_IDLE_TIME', 30),
+        ],
+        'options' => [
+            'tcp_keepalive' => 1,
+            'tcp_nodelay' => true,
         ],
     ],
 ];
